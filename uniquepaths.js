251c9978c265535bc5e87d3358ebae1b4b12736f
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2022-05-11 20:58:27 -0400

    Create uniquepaths.js

---
+++ uniquepaths.js
@@ -0,0 +1,16 @@
+var uniquePaths = function(m, n) {
+    let dp = []
+        
+    for(let i =0; i<n; i++)
+        {
+            dp.push([...new Array(m).fill(1)])
+        }
+    for(let i = 1; i< n; i++)
+        {
+            for(let j = 1; j<m; j++)
+                {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                }
+        }
+    return dp[n-1][m-1]
+};
